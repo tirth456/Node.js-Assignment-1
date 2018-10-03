fd53e45ca1cbdc92c4feb864b0bc8ecc4a7e8e90
Tirthrajsinh Chauhan <tirthraj@Tirthrajsinhs-MacBook-Pro.local>
2018-10-02 20:59:08 -0400

    added GET,POST method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var SERVER_NAME = 'user-api'
+var SERVER_NAME = 'Assignment-1'
 var PORT = 8000;
 var HOST = '127.0.0.1';
 
@@ -6,16 +6,16 @@ var HOST = '127.0.0.1';
 var restify = require('restify')
 
   // Get a persistence engine for the users
-  , usersSave = require('save')('users')
+  , usersSave = require('save')('sendGet')
 
   // Create the restify server
   , server = restify.createServer({ name: SERVER_NAME})
 
   server.listen(PORT, HOST, function () {
   console.log('Server %s listening at %s', server.name, server.url)
-  console.log('Resources:')
-  console.log(' /users')
-  console.log(' /users/:id')  
+  console.log('Endpoints:')
+  console.log(' http://127.0.0.1:3000/sendGet method: GET')
+  console.log(' http://127.0.0.1:3000/sendPost method: POST')  
 })
 
 server
@@ -26,28 +26,28 @@ server
   .use(restify.bodyParser())
 
 // Get all users in the system
-server.get('/users', function (req, res, next) {
+server.get('/sendGet', function (req, res, next) {
 
   // Find every entity within the given collection
-  usersSave.find({}, function (error, users) {
+  usersSave.find({}, function (error, sendGet) {
 
     // Return all of the users in the system
-    res.send(users)
+    res.send(sendGet)
   })
 })
 
 // Get a single user by their user id
-server.get('/users/:id', function (req, res, next) {
+server.get('/sendGet/:id', function (req, res, next) {
 
   // Find a single user by their id within save
-  usersSave.findOne({ _id: req.params.id }, function (error, user) {
+  usersSave.findOne({ _id: req.params.id }, function (error, sendGet) {
 
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
-    if (user) {
+    if (sendGet) {
       // Send the user if no issues
-      res.send(user)
+      res.send(sendGet)
     } else {
       // Send 404 header if the user doesn't exist
       res.send(404)
@@ -56,7 +56,7 @@ server.get('/users/:id', function (req, res, next) {
 })
 
 // Create a new user
-server.post('/users', function (req, res, next) {
+server.post('/sendPost', function (req, res, next) {
 
   // Make sure name is defined
   if (req.params.name === undefined ) {
@@ -84,7 +84,7 @@ server.post('/users', function (req, res, next) {
 })
 
 // Update a user by their id
-server.put('/users/:id', function (req, res, next) {
+server.put('/sendGet/:id', function (req, res, next) {
 
   // Make sure name is defined
   if (req.params.name === undefined ) {
@@ -103,7 +103,7 @@ server.put('/users/:id', function (req, res, next) {
 	}
   
   // Update the user with the persistence engine
-  usersSave.update(newUser, function (error, user) {
+  usersSave.update(newUser, function (error, sendGet) {
 
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
@@ -114,10 +114,10 @@ server.put('/users/:id', function (req, res, next) {
 })
 
 // Delete user with the given id
-server.del('/users/:id', function (req, res, next) {
+server.del('/sendGet/:id', function (req, res, next) {
 
   // Delete the user with the persistence engine
-  usersSave.delete(req.params.id, function (error, user) {
+  usersSave.delete(req.params.id, function (error, sendGet) {
 
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
